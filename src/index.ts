a6bf8bb3bcd9c3a99c2850f3c64b23030968ee96
safwat-fathi <safwat.rashwan@gmail.com>
2023-12-29 15:58:36 +0200

    add: called once leetcode problem

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { lengthOfLongestSubstring } from "./leetcode/HashMaps/longest-substring-without-repeating-characters";
+// import { lengthOfLongestSubstring } from "./leetcode/HashMaps/*longest-substring-without-repeating-characters";
 
-const s1 = "abcabcbb";
-const s2 = "bbbbb";
-const s3 = "pwwkew";
-const s4 = " ";
-const s5 = "aab";
-const s6 = "dvdf";
+// const s1 = "abcabcbb";
+// const s2 = "bbbbb";
+// const s3 = "pwwkew";
+// const s4 = " ";
+// const s5 = "aab";
+// const s6 = "dvdf";
 
-console.log(lengthOfLongestSubstring(s6));
+// console.log(lengthOfLongestSubstring(s6));
 // lengthOfLongestSubstring(s1);
---
+++ src/leetcode/allow-one-function-call/index.ts
@@ -0,0 +1,29 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined;
+
+export const once = (fn: Function): OnceFn => {
+  let called = false;
+
+  return function (...args) {
+    if (!called) {
+      called = true;
+      return fn(...args);
+    }
+
+    return undefined;
+  };
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
