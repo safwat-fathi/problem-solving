9b946737a497dad68d6242550f38616043429187
Safwat Fathi <safwat.fathi@jobzella.com>
2024-01-04 13:08:12 +0200

    remove: unused code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 // import { lengthOfLongestSubstring } from "./leetcode/HashMaps/*longest-substring-without-repeating-characters";
 
-import { maxProfit } from "./leetcode/*Arrays/best-time-to-buy-and-sell-stock";
-
 // const s1 = "abcabcbb";
 // const s2 = "bbbbb";
 // const s3 = "pwwkew";
@@ -14,5 +12,3 @@ import { maxProfit } from "./leetcode/*Arrays/best-time-to-buy-and-sell-stock";
 const p1 = [2, 1, 2, 1, 0, 1, 2]; // 2
 const p2 = [7, 1, 5, 3, 6, 4]; // 5
 const p3 = [7, 6, 4, 3, 1]; // 0
-
-console.log(maxProfit(p3));
