5375281d6b4c6d61417f382ea1cf9b6b8aa86e15
Safwat Fathi <safwat.fathi@jobzella.com>
2024-01-04 14:13:49 +0200

    add: queue & dequeue DS

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import { lengthOfLongestSubstring } from "./leetcode/HashMaps/*longest-substring-without-repeating-characters";
 
-import { maxSlidingWindow } from "./leetcode/*Arrays/sliding-window-maximum";
+import { maxSlidingWindow } from "./leetcode/*Arrays/*sliding-window-maximum";
+import { Queue } from "./leetcode/Queue/queue";
 
 // const s1 = "abcabcbb";
 // const s2 = "bbbbb";
@@ -16,4 +17,13 @@ const n1 = [1, 3, -1, -3, 5, 3, 6, 7];
 const n2 = [1, 2, 3, 4, 5];
 const n3 = [1];
 
-console.log(maxSlidingWindow(n1, 3));
+// console.log(maxSlidingWindow(n1, 3));
+
+const q = new Queue<number>();
+
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+
+console.log(q.dequeue());
+console.log(q.peek());
